cd3170cc6c1701c8d8fbc7e67e78ff2247428a3d
Steve Stevens <Steve.Stevens@live.co.uk>
2017-04-01 11:33:29 +0100

    select all tvshow data

--- app/src/main/java/uk/co/ourfriendirony/medianotifier/general/TVShowDatabase.java
+++ app/src/main/java/uk/co/ourfriendirony/medianotifier/general/TVShowDatabase.java
@@ -11,6 +11,8 @@ import uk.co.ourfriendirony.medianotifier.autogen.tvshow.MDLookupTVShow;
 import uk.co.ourfriendirony.medianotifier.autogen.tvshow.MDSeason;
 import uk.co.ourfriendirony.medianotifier.clients.MovieDatabaseClient;
 
+import static uk.co.ourfriendirony.medianotifier.general.TVShowDatabaseDefinition.*;
+
 public class TVShowDatabase {
 
     private final SQLiteDatabase dbWritable;
@@ -22,12 +24,26 @@ public class TVShowDatabase {
     }
 
     public String selectTVShow() {
-        ContentValues tvShowRow = new ContentValues();
+        String sql = "SELECT * FROM " + TVShowDatabaseDefinition.TABLE_TVSHOWS + ";";
+        StringBuilder result = new StringBuilder();
+        Cursor cursor = dbReadable.rawQuery(sql, null);
+        try {
+            while (cursor.moveToNext()) {
+                result.append(getColumnValue(cursor, TT_ID)).append(" | ")
+                        .append(getColumnValue(cursor, TT_DATE)).append(" | ")
+                        .append(getColumnValue(cursor, TT_TITLE)).append(" | ")
+                        .append(getColumnValue(cursor, TT_OVERVIEW))
+                        .append("\n*****************\n");
+            }
+        } finally {
+            cursor.close();
+        }
+        return result.toString();
 
-        Cursor cursor = dbReadable.rawQuery("SELECT * FROM " + TVShowDatabaseDefinition.TABLE_TVSHOWS, null);
-        cursor.moveToFirst();
-        return cursor.getString(cursor.getColumnIndex(TVShowDatabaseDefinition.TT_OVERVIEW));
-     
+    }
+
+    private String getColumnValue(Cursor cursor, String field) {
+        return cursor.getString(cursor.getColumnIndex(field));
     }
 
     public void saveTVShow(int tvShowId) {
@@ -50,30 +66,30 @@ public class TVShowDatabase {
 
     private void insertTVShow(MDLookupTVShow tvShow) {
         ContentValues tvShowRow = new ContentValues();
-        tvShowRow.put(TVShowDatabaseDefinition.TT_ID, tvShow.getId());
-        tvShowRow.put(TVShowDatabaseDefinition.TT_TITLE, tvShow.getName());
-        tvShowRow.put(TVShowDatabaseDefinition.TT_IMDB, "");
-        tvShowRow.put(TVShowDatabaseDefinition.TT_DATE, tvShow.getFirstAirDate());
-        tvShowRow.put(TVShowDatabaseDefinition.TT_OVERVIEW, tvShow.getOverview());
-        dbWritable.insert(TVShowDatabaseDefinition.TABLE_TVSHOWS, null, tvShowRow);
+        tvShowRow.put(TT_ID, tvShow.getId());
+        tvShowRow.put(TT_TITLE, tvShow.getName());
+        tvShowRow.put(TT_IMDB, "");
+        tvShowRow.put(TT_DATE, tvShow.getFirstAirDate());
+        tvShowRow.put(TT_OVERVIEW, tvShow.getOverview());
+        dbWritable.insert(TABLE_TVSHOWS, null, tvShowRow);
     }
 
     private void insertTVShowSeason(MDSeason season) {
         ContentValues seasonRow = new ContentValues();
-        seasonRow.put(TVShowDatabaseDefinition.TTS_ID, season.getId());
-        seasonRow.put(TVShowDatabaseDefinition.TTS_SEASON_NO, season.getSeasonNumber());
-        seasonRow.put(TVShowDatabaseDefinition.TTS_DATE, season.getAirDate());
-        dbWritable.insert(TVShowDatabaseDefinition.TABLE_TVSHOWS_SEASONS, null, seasonRow);
+        seasonRow.put(TTS_ID, season.getId());
+        seasonRow.put(TTS_SEASON_NO, season.getSeasonNumber());
+        seasonRow.put(TTS_DATE, season.getAirDate());
+        dbWritable.insert(TABLE_TVSHOWS_SEASONS, null, seasonRow);
     }
 
     private void insertTVShowEpisode(MDEpisode episode) {
         ContentValues episodeRow = new ContentValues();
-        episodeRow.put(TVShowDatabaseDefinition.TTSE_ID, episode.getId());
-        episodeRow.put(TVShowDatabaseDefinition.TTSE_SEASON_NO, episode.getSeasonNumber());
-        episodeRow.put(TVShowDatabaseDefinition.TTSE_EPISODE_NO, episode.getEpisodeNumber());
-        episodeRow.put(TVShowDatabaseDefinition.TTSE_TITLE, episode.getName());
-        episodeRow.put(TVShowDatabaseDefinition.TTSE_DATE, episode.getAirDate());
-        episodeRow.put(TVShowDatabaseDefinition.TTSE_OVERVIEW, episode.getOverview());
-        dbWritable.insert(TVShowDatabaseDefinition.TABLE_TVSHOWS_EPISODES, null, episodeRow);
+        episodeRow.put(TTSE_ID, episode.getId());
+        episodeRow.put(TTSE_SEASON_NO, episode.getSeasonNumber());
+        episodeRow.put(TTSE_EPISODE_NO, episode.getEpisodeNumber());
+        episodeRow.put(TTSE_TITLE, episode.getName());
+        episodeRow.put(TTSE_DATE, episode.getAirDate());
+        episodeRow.put(TTSE_OVERVIEW, episode.getOverview());
+        dbWritable.insert(TABLE_TVSHOWS_EPISODES, null, episodeRow);
     }
 }
