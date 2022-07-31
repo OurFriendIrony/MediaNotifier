64ed69766a920214fc3e13ccde4e1299147dc0d7
Steve Stevens <steve.h.i.stevens@gmail.com>
2022-07-31 14:42:19 +0100

    java11

--- app/src/main/java/uk/co/ourfriendirony/medianotifier/clients/rawg/RAWGClient.java
+++ app/src/main/java/uk/co/ourfriendirony/medianotifier/clients/rawg/RAWGClient.java
@@ -22,9 +22,8 @@ public class RAWGClient extends AbstractClient {
     private static final String HOST = "https://api.rawg.io/api/";
     private static final String URL_PAGE = "&page=1&page_size=20";
     private static final String URL_API = "key=" + API_KEY;
-    private static final String URL_GAME_QUERY = HOST + "games?search=@NAME@" + URL_PAGE + EXCLUDE_ADDITIONS + "&" + URL_API;
+    private static final String URL_GAME_QUERY = HOST + "games?search=@NAME@" + URL_PAGE + "&exclude_additions" + "&" + URL_API;
     private static final String URL_GAME_ID = HOST + "games/@ID@" + "?" + URL_API;
-    private static final String EXCLUDE_ADDITIONS = "&exclude_additions";
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     private String payload;
